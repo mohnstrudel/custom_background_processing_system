8dc9dfdcd57e0dc7c993f57b7deb39507dc77e86
Anton Kostin <anton.kostin@mhm-hr.com>
2019-04-05 14:10:44 +0200

    CVPS-002: Full async Thread implementation without Redis caching.

--- magique.rb
+++ magique.rb
@@ -1,4 +1,30 @@
 module Magique
+  def self.backend
+    @backend
+  end
+
+  def self.backend=(backend)
+    @backend = backend
+  end
+
+  class Processor
+    def self.start(concurrency = 1)
+      concurrency.times { |n| new("Processor #{n}")}
+    end
+
+    def initialize(name)
+      thread = Thread.new do
+        loop do
+          payload = Magique.backend.pop
+          worker_class = payload[:worker]
+          worker_class.new.perform(*payload[:args])
+        end
+      end
+
+      thread.name = name
+    end
+  end
+
   module Worker
     def self.included(base)
       base.extend(ClassMethods)
@@ -7,6 +33,12 @@ module Magique
     module ClassMethods
       def perform_now(*args)
         new.perform(*args)
+      end
+
+      def perform_async(*args)
+        # Args sind hier in dem Fall die URLs
+        Magique.backend.push(worker: self, args: args)
+        # Thread.new { new.perform(*args) }
       end
     end
 
--- run.rb
+++ run.rb
@@ -2,16 +2,31 @@ require 'open-uri'
 require 'nokogiri'
 require_relative 'magique'
 
+RUBYMAGIC = %w(https://rbc.ru https://gamestar.de https://atomic-digital.ru https://overclockers.ru https://focus.de https://web.de)
+
 class TitleExtractorService
   include Magique::Worker
 
-  def call(url)
+  def perform(url)
     document = Nokogiri::HTML(open(url))
     title = document.css('html > head > title').first.content
-    puts title.gsub(/[[:space:]]+/, ' ').strip
+    puts "[#{Thread.current.name}] #{title.gsub(/[[:space:]]+/, ' ').strip}"
   rescue
     puts "Unable to find a title for #{url}"
   end
 end
 
-TitleExtractorService.new.perform_now
+Magique.backend = Queue.new
+Magique::Processor.start(5)
+
+RUBYMAGIC.each do |url|
+  TitleExtractorService.perform_async(url)
+  sleep 0.5
+end
+
+
+
+# Aktuell im Schnitt ca. so:
+#        user     system      total        real
+# URL  0.190000   0.020000   0.210000 (  1.318162)
+# Average: 1.3707450999936555
